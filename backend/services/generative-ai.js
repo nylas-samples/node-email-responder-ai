5898b21eb94db9d1bb4daced91e2eeaa272a00b4
Ram Bansal <ram.bansal@nylas.com>
2023-07-26 11:15:43 +0200

    updates: - updated generative-ai to include multipleCalls method - and remove extra text from model response

--- backend/services/generative-ai.js
+++ backend/services/generative-ai.js
@@ -5,6 +5,8 @@ dotenv.config({ path: __dirname + '/../.env' });
 
 const hf = new HfInference(process.env.HUGGINGFACE_HUB_API_KEY);
 
+const retrieveAssistantResponse = str => str.trim().split('|>')[3];
+
 exports.generateEmailResponseGpt = async(startOfEmailResponse) => {
   const answer = await hf.textGeneration({
     model: "gpt2",
@@ -15,14 +17,35 @@ exports.generateEmailResponseGpt = async(startOfEmailResponse) => {
   return answer;
 }
 
+const multipleCalls = async(answer) => {
+  let count = 5;
+
+  while(true) {
+    let newAnswer = await hf.textGeneration({
+      model: "OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5",
+      inputs: answer.generated_text,
+    });
+
+    // Loop escape hatch
+    if(answer.generated_text === newAnswer.generated_text || count <= 1) break;
+
+    answer = newAnswer;
+    count--;
+  }
+
+  return answer;
+}
+
 exports.generateEmailResponse = async(emailBody) => {
   const answer = await hf.textGeneration({
     model: "OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5",
     inputs: `<|prompter|>Respond to the following email: ${emailBody}<|endoftext|><|assistant|>`,
   });
 
-  console.log(answer);
-  return answer;
+  const response = await multipleCalls(answer);
+
+  console.log(response);
+  return retrieveAssistantResponse(response.generated_text);
 }
 
 // this.generateEmailResponseGpt("Let's grab a coffee on");
