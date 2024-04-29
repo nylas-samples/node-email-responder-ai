a033d1f126479a64416053b8f6c73555c7d64520
Ram <553578+relaxedtomato@users.noreply.github.com>
2024-04-29 15:48:47 -0400

    migrate to api v3

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const mockDb = require('./utils/mock-db');
-const route = require('./route');
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import mockDb from './utils/mock-db.js';
+import route from './route.js'
 
-const Nylas = require('nylas');
-const { WebhookTriggers } = require('nylas/lib/models/webhook');
-const { Scope } = require('nylas/lib/models/connect');
-const { openWebhookTunnel } = require('nylas/lib/services/tunnel');
-
-dotenv.config();
+import Nylas from 'nylas';
 
 const app = express();
 
@@ -19,18 +14,21 @@ app.use(cors());
 // The port the express app will run on
 const port = 9000;
 
+
 // Initialize the Nylas SDK using the client credentials
-Nylas.config({
-  clientId: process.env.NYLAS_CLIENT_ID,
-  clientSecret: process.env.NYLAS_CLIENT_SECRET,
-  apiServer: process.env.NYLAS_API_SERVER,
-});
+const NylasConfig = {
+  apiKey: process.env.NYLAS_API_KEY,
+  apiUri: process.env.NYLAS_API_REGION_URI,
+};
+
+const nylas = new Nylas(NylasConfig);
 
 // Before we start our backend, we should register our frontend as a redirect
 // URI to ensure the auth completes
 const CLIENT_URI =
   process.env.CLIENT_URI || `http://localhost:${process.env.PORT || 3000}`;
-Nylas.application({
+
+nylas.applications.getDetails({
   redirectUris: [CLIENT_URI],
 }).then((applicationDetails) => {
   console.log(
@@ -38,61 +36,48 @@ Nylas.application({
     JSON.stringify(applicationDetails)
   );
 });
-
-// Start the Nylas webhook
-openWebhookTunnel({
-  // Handle when a new message is created (sent)
-  onMessage: function handleEvent(delta) {
-    switch (delta.type) {
-      case WebhookTriggers.MessageCreated:
-        console.log(
-          'Webhook trigger received, message created. Details: ',
-          JSON.stringify(delta.objectData, undefined, 2)
-        );
-        break;
-      case WebhookTriggers.AccountConnected:
-        console.log(
-          'Webhook trigger received, account connected. Details: ',
-          JSON.stringify(delta.objectData, undefined, 2)
-        );
-        break;
-    }
-  },
-}).then((webhookDetails) => {
-  console.log('Webhook tunnel registered. Webhook ID: ' + webhookDetails.id);
-});
+  
 
 // '/nylas/generate-auth-url': This route builds the URL for
 // authenticating users to your Nylas application via Hosted Authentication
 app.post('/nylas/generate-auth-url', express.json(), async (req, res) => {
   const { body } = req;
 
-  const authUrl = Nylas.urlForAuthentication({
+  const authUrl = nylas.auth.urlForOAuth2({
+    clientId: process.env.NYLAS_CLIENT_ID,
+    redirectUri: CLIENT_URI,
     loginHint: body.email_address,
-    redirectURI: (CLIENT_URI || '') + body.success_url,
-    scopes: [Scope.EmailModify, Scope.EmailSend],
-  });
+    scopes: [
+      'openid',
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/gmail.modify',
+      'https://www.googleapis.com/auth/gmail.send',
+    ]
+  })
 
   return res.send(authUrl);
 });
 
 // '/nylas/exchange-mailbox-token': This route exchanges an authorization
-// code for an access token
+// code for an grant Id
 // and sends the details of the authenticated user to the client
 app.post('/nylas/exchange-mailbox-token', express.json(), async (req, res) => {
   const body = req.body;
 
-  const { accessToken, emailAddress } = await Nylas.exchangeCodeForToken(
-    body.token
-  );
+  const { grantId, email } = await nylas.auth.exchangeCodeForToken({ 
+    clientSecret: process.env.NYLAS_API_KEY,
+    clientId: process.env.NYLAS_CLIENT_ID,
+    code: body.token,
+    redirectUri: CLIENT_URI,
+  });
 
-  // Normally store the access token in the DB
-  console.log('Access Token was generated for: ' + emailAddress);
+  // Normally store the grant Id in the DB
+  // console.log('Grant Id was generated for: ', response);
 
   // Replace this mock code with your actual database operations
-  const user = await mockDb.createOrUpdateUser(emailAddress, {
-    accessToken,
-    emailAddress,
+  const user = await mockDb.createOrUpdateUser(email, {
+    emailAddress: email,
+    grantId,
   });
 
   // Return an authorization object to the user
@@ -108,7 +93,7 @@ async function isAuthenticated(req, res, next) {
     return res.status(401).json('Unauthorized');
   }
 
-  // Query our mock db to retrieve the stored user access token
+  // Query our mock db to retrieve the stored user grant Id
   const user = await mockDb.findUser(req.headers.authorization);
 
   if (!user) {
@@ -132,6 +117,10 @@ app.get('/nylas/read-emails', isAuthenticated, (req, res) =>
 
 app.get('/nylas/message', isAuthenticated, async (req, res) => {
   route.getMessage(req, res);
+});
+
+app.get('/nylas/messages', isAuthenticated, async (req, res) => {
+  route.getMessages(req, res);
 });
 
 app.get('/nylas/file', isAuthenticated, async (req, res) => {
